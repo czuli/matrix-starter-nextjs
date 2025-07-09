c900f247619efdcc83cf893999b925064a99fa14
Łukasz Czulak <lukasz.czulak@gmail.com>
2025-07-09 16:19:44 +0200

    change text

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <h2 className="my-4">Hi Buddy!</h2>
+        <h2 className="my-4 text-2xl text-blue-200">Hello World!</h2>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
