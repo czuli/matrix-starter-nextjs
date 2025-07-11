6c6bc28af98dddbea3adb64bf50a30c0bae4fdb4
Lucas Czulak <lucas.czulak@buddy.works>
2025-07-11 15:37:58 +0200

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export'
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
